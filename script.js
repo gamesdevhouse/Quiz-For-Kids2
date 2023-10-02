2d4fd732f87ffbfe3ee0a9617f22861a7f46802a
GlennJo <139962883+GlennJohansson85@users.noreply.github.com>
2023-10-02 19:50:59 +0200

    script.js
    -Add 'document.addEventListener("DOMContentLoaded",) function ()' = Ensure js load after html.
    
    -Add 10 questions, 4 answer options/question, 1 correct answer option/question, a placeholder/question

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
-// Function - Ensure JS runs after HTML
+// Function - Ensure JavaScript runs after HTML
+document.addEventListener("DOMContentLoaded",) function () {
+    
+    // 10 quiz questions, 4 answer options/questions, 1 correct answer option/question, a placeholder/question
+    let questions=[
+        {
+            question: "what is the largest animal in the world?",
+            options: ["giraffe", "brown bear", "elephant", "blue whale"],
+            answer: "blue whale",
+            userAnswer: null
+        },
+        {
+            question: "what is the name of the world´s highest mountain?",
+            options: ["alps", "zugspitze", "mount everest", "annapurna"],
+            answer: "mount everest",
+            userAnswer: null
+        },
+        {
+            question: "how many wings does a butterfly have?",
+            options: ["4", "2", "8", "6"],
+            answer: "4",
+            userAnswer: null
+        },
+        {
+            question: "at what age do you become a teenager?",
+            options: ["10", "12", "15", "13"],
+            answer: "13",
+            userAnswer: null
+        },
+        {
+            question: "what is the capital city of scotland?",
+            options: ["edinburgh", "glasgow", "toronto", "london"],
+            answer: "edinburgh",
+            userAnswer: null
+        },
+        {
+            question: "how many players are there in a fotball team?",
+            options: ["8", "10", "11", "12"],
+            answer: "11",
+            userAnswer: null
+        },
+        {
+            questions: "a lobster has how many legs?",
+            options: ["8", "20", "12", "10"],
+            answer: "10",
+            userAnswer: null
+        },
+        {
+            question: "how long is one hour in minutes?",
+            options: ["60", "100", "80", "90"],
+            answer: "60",
+            userAnswer: null
+        },
+        {
+            question: "what date is christmas eve?",
+            options: ["21th december", "22th december", "23th december", "24th december"],
+            answer: "24th december",
+            userAnswer: null
+        },
+        {
+            question: "which country has the largest population in the world",
+            options: ["usa", "china", "japan", "india"],
+            answer: "china",
+            userAnswer: null
+        }   
+];
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
